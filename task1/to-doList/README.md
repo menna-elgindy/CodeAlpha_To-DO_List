2149ae8299b6df73aea6ecbac807cfd26509bc72
menna elgindy <mennaelgindy98@gmail.com>
2024-05-14 21:39:27 +0300

    1st commit

---
+++ task1/to-doList/README.md
@@ -0,0 +1,18 @@
+# TO-DO list web application
+
+TO-DO list web application i a simple app built using html,css and javascript. Users can easily create,
+save and organize their daily tasks. This app smplifies task management and provides a convenient way to 
+track and prioritize tasks.
+
+## features:
+- Add new task.
+- Check completed tasks as done.
+- Delete task.
+- Save tasks list.
+
+## Usage:
+
+- Add task : By writing your task in the input field then clicking on add button.
+- Check done : By clicking on the check circle existing left the task you want to mark it done.
+- Delete task : By clicking on the cross mark existing right the task you want to delete.
+- Save task : By clicking on the save button , your list is saved at your browser's local storge then loaded every time you visit the   web app.
---
+++ task1/to-doList/script.js
@@ -0,0 +1,65 @@
+var taskValue = document.getElementById("taskValue");
+var list = document.getElementById("list");
+var savedTasks =JSON.parse(localStorage.getItem("savedTasks"))||[];
+
+function checkDone(task){
+    task.addEventListener("click",function(){
+        task.classList.toggle("done");
+    })
+}
+
+function deleteTask(task){
+    // creating deleting btn for every task
+    let deleteBtn = document.createElement("span");
+    deleteBtn.className = "deleteBtn";
+    deleteBtn.innerHTML = "✕";
+    task.appendChild(deleteBtn);
+    // deleting task
+    task.querySelector("span").addEventListener("click",function(){
+        savedTasks = savedTasks.filter((saved) => saved != task.textContent.slice(0, -1));
+        task.remove();
+    })
+ }
+
+
+
+function newTask(){
+    //creating new task element
+    let task = document.createElement("li");
+    if(taskValue.value ==""){
+        window.alert("please write your new task");
+    }else {  
+        task.innerHTML= taskValue.value;
+        savedTasks.push(taskValue.value);
+        taskValue.value="";
+        // checking task as done
+        checkDone(task);
+        // deleting task
+        deleteTask(task);
+
+        list.appendChild(task);}
+}
+
+function saveTasks(){
+    localStorage.setItem("savedTasks",JSON.stringify(savedTasks))
+}
+
+function loadSavedTasks(){
+    savedTasks.forEach(element => {
+        let task = document.createElement("li");
+        task.innerHTML= element;
+        // checking task as done
+        checkDone(task);
+        // deleting task
+        deleteTask(task);
+
+        list.appendChild(task);
+    })
+        
+    //clear localstorge after loading saved tasks
+    localStorage.setItem("savedTasks","");
+     localStorage.clear();  
+}
+
+document.addEventListener("DOMContentLoaded",loadSavedTasks)
+
